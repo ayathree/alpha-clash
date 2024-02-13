34be0a10a39d702de2bb6cbe2d43c1ac716a8b55
ayathree <nobanitaayathree333@gmail.com>
2024-02-14 01:35:14 +0600

    added bg in krypad

--- script/alpha.js
+++ script/alpha.js
@@ -1,8 +1,23 @@
-function play() {
-    const homeScreen = document.getElementById('home-screen');
-    homeScreen.classList.add('hidden');
+// function play() {
+//     const homeScreen = document.getElementById('home-screen');
+//     homeScreen.classList.add('hidden');
 
-    const playGround = document.getElementById('play-ground');
-    playGround.classList.remove('hidden');
+//     const playGround = document.getElementById('play-ground');
+//     playGround.classList.remove('hidden');
+    
+// }
+function continueGame() {
+    const alphabet = getARandomAlphabet();
+    console.log('your random alphabet', alphabet)
+    const currentAlphabet = document.getElementById('current-alpha');
+    currentAlphabet.innerText = alphabet;
+
+    addBg(alphabet);
+}
+function play() {
+    hideElement('home-screen');
+    showElement('play-ground');
+    continueGame();
+
     
 }
---
+++ script/dublicate.js
@@ -0,0 +1,33 @@
+function hideElement(elementId) {
+    const element = document.getElementById(elementId);
+    element.classList.add('hidden');
+    
+}
+
+function showElement(elementId) {
+    const element = document.getElementById(elementId);
+    element.classList.remove('hidden');
+    
+}
+
+function addBg(elementId) {
+    const element = document.getElementById(elementId);
+    element.classList.add('bg-orange-400');
+    
+}
+
+function getARandomAlphabet() {
+    const alphabetString = 'abcdefghijklmnopqrstuvwxyz';
+    const alphabets = alphabetString.split('');
+    // console.log(alphabets);
+
+    const randomNumber = Math.random()*25;
+    const index = Math.round(randomNumber);
+    
+
+    const alphabet = alphabets[index];
+    // console.log(index, alphabet);
+    return alphabet;
+
+    
+}
