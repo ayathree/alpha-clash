b6bedd880b81e86c8d108a2c2a0dffaeae9d0d3a
ayathree <nobanitaayathree333@gmail.com>
2024-02-14 21:30:54 +0600

    added the key function

--- script/alpha.js
+++ script/alpha.js
@@ -6,6 +6,34 @@
 //     playGround.classList.remove('hidden');
     
 // }
+
+// keyboard connection
+function handleKeyboardButtonPress(event) {
+    const playPressed = event.key;
+    // get what i have press
+    console.log( 'i have pressed',playPressed);
+
+    // get the expected press
+    const currentAlphabetElement = document.getElementById('current-alpha');
+    const currentAlphabet = currentAlphabetElement.innerText;
+    const expectedAlphabet = currentAlphabet.toLowerCase();
+    console.log(playPressed, expectedAlphabet);
+
+    // check matched or not
+    if(playPressed === expectedAlphabet){
+        console.log('you win');
+        console.log('you have pressed correctly', expectedAlphabet);
+        // for remove the background color of pressed key
+        removeBg(expectedAlphabet);
+        continueGame();
+    }
+    else{
+        console.log('you lose');
+    }
+    
+}
+document.addEventListener('keyup', handleKeyboardButtonPress);
+
 function continueGame() {
     const alphabet = getARandomAlphabet();
     console.log('your random alphabet', alphabet)
--- script/dublicate.js
+++ script/dublicate.js
@@ -16,6 +16,14 @@ function addBg(elementId) {
     
 }
 
+function removeBg(elementId) {
+    const element = document.getElementById(elementId);
+    element.classList.remove('bg-orange-400');
+    
+}
+
+
+
 function getARandomAlphabet() {
     const alphabetString = 'abcdefghijklmnopqrstuvwxyz';
     const alphabets = alphabetString.split('');
